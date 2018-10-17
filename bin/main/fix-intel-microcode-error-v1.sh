5d06eee1fb1317544f276e1730e9a14a9c56574b
Erik Dubois <erik.dubois@gmail.com>
2018-10-17 08:29:38 +0200

    more help in scripts

--- bin/main/fix-intel-microcode-error-v1.sh
+++ bin/main/fix-intel-microcode-error-v1.sh
@@ -13,6 +13,8 @@ set -e
 #
 ##################################################################################################################
 
+# you can use lscpu in the terminal to know if you have an intel or amd
+
 echo "Run this if you see an error in your bootup screen or dmesg about microcode"
 
 sudo pacman -S intel-ucode --noconfirm
