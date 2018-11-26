bc94d130ce32f5d73e30c8498b57b6fc59683339
Erik Dubois <erik.dubois@gmail.com>
2018-11-26 11:26:45 +0100

    initial start with samba script

---
+++ bin/main/delete-all-samba-related-services-and-apps-error-message.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -e
+##################################################################################################################
+# Author	:	Erik Dubois
+# Website	:	https://www.erikdubois.be
+# Website	:	https://www.arcolinux.info
+# Website	:	https://www.arcolinux.com
+# Website	:	https://www.arcolinuxd.com
+# Website	:	https://www.arcolinuxforum.com
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+echo "################################################################"
+echo "Disabling services"
+echo "################################################################"
+
+sudo systemctl disable smb.service
+sudo systemctl disable nmb.service
+sudo systemctl disable avahi-daemon.service
+sudo systemctl disable winbind.service
+
+echo "################################################################"
+echo "Uninstalling packages"
+echo "################################################################"
+
+sudo pacman -R samba
+#sudo pacman -R avahi
+sudo pacman -R nss-mdns
+sudo pacman -R gvfs-samba
+
+echo "################################################################"
+echo "Deleting ArcoLinux config files"
+echo "################################################################"
+
+sudo rm -r /etc/samba/smb.conf
+sudo rm -r /etc/samba/smb.conf.orginal
+
+echo "################################################################"
+echo "###  Samba and all related packages and services are gone   ####"
+echo "################################################################"
