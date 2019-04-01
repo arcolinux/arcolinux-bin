4684bf912c919a7022b2d77bc60c2b4ccd7764dd
Erik Dubois <erik.dubois@gmail.com>
2019-04-01 11:16:18 +0200

    square and keep rolling

---
+++ etc/skel/.bin/stay-rolling/19.3-to-19.4/stay-rolling-v1.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#set -e
+##################################################################################################################
+# Author	:	Erik Dubois
+# Website	:	https://www.erikdubois.be
+# Website	:	https://www.arcolinux.info
+# Website	:	https://www.arcolinux.com
+# Website	:	https://www.arcolinuxd.com
+# Website	:	https://www.arcolinuxforum.com
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+echo "#############################"
+echo "REMOVALS"
+echo "#############################"
+echo "Removing bluez-firmware - moved to AUR"
+
+sudo pacman -Rs bluez-firmware
+
+echo
+echo "#############################"
+echo "INSTALLATIONS"
+echo "#############################"
+echo "We have installed these packages on the iso :"
+echo
+echo "NONE"
+echo
+echo "################################################################"
+echo "###                    All done                             ####"
+echo "################################################################"
