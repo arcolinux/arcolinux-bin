f766e893ce72ebaca81e9402681b841a381cc687
Erik Dubois <erik.dubois@gmail.com>
2019-05-07 19:23:21 +0200

    stayrolling for 19.05

---
+++ etc/skel/.bin/stay-rolling/19.4-to-19.5/stay-rolling-v1.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#set -e
+##################################################################################################################
+# Author	:	Erik Dubois
+# Website	:	https://www.erikdubois.be
+# Website	:	https://www.arcolinux.info
+# Website	:	https://www.arcolinux.com
+# Website	:	https://www.arcolinuxd.com
+# Website	:	https://www.arcolinuxforum.com
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+echo "#############################"
+echo "REMOVALS"
+echo "#############################"
+echo "Removing reshift still issues with geoip"
+
+sudo pacman -Rs redshift
+
+echo "Removing wpa_actiond - moved to AUR and is orphaned"
+
+sudo pacman -Rs wpa_actiond
+
+echo "Removing oomox - build takes super long and installs lot of software"
+echo "New PKGBUILD created by maintainer"
+echo "Decide if you want to keep it or not"
+
+sudo pacman -Rs oomox
+
+echo "Reinstalling packages we do want to keep after deleting oomox"
+
+sudo pacman -S gtk-engines --noconfirm
+sudo pacman -S optipng --noconfirm
+
+echo "Removing old Arc themes and adding new Arc themes from Nico Hood"
+
+sudo pacman -S arcolinux-arc-themes-nico-git
+
+echo
+echo "#############################"
+echo "INSTALLATIONS"
+echo "#############################"
+echo "We have installed these packages on the iso :"
+echo
+echo "NONE"
+echo
+echo "################################################################"
+echo "###                    All done                             ####"
+echo "################################################################"
