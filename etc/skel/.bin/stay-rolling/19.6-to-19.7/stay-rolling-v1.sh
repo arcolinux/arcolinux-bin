afeec47a678eec9e0bf5e35ee6f236d0f05f15d7
Erik Dubois <erik.dubois@gmail.com>
2019-06-24 06:54:36 +0200

    typo in release

--- etc/skel/.bin/stay-rolling/19.6-to-19.7/stay-rolling-v1.sh
+++ etc/skel/.bin/stay-rolling/19.6-to-19.7/stay-rolling-v1.sh
@@ -42,7 +42,7 @@ echo "##########################################"
 echo "CHANGING VERSION IN /ETC/LSB-RELEASE"
 echo "##########################################"
 
-sudo sed -i 's/v19.05.1/v19.07.4/g' /etc/lsb-release
+sudo sed -i 's/v19.06.1/v19.07.5/g' /etc/lsb-release
 
 echo "################################################################"
 echo "###                   LSB-RELEASE DONE                       ####"
