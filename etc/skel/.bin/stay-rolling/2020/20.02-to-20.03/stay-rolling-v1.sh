b2665ff6c4c081b2eef2cb9111b5a2efbeb2ef50
Erik Dubois <erik.dubois@gmail.com>
2020-03-04 16:07:50 +0100

    new version nr that is it

--- etc/skel/.bin/stay-rolling/2020/20.02-to-20.03/stay-rolling-v1.sh
+++ etc/skel/.bin/stay-rolling/2020/20.02-to-20.03/stay-rolling-v1.sh
@@ -87,7 +87,7 @@ echo "##########################################################################
 echo "CHANGING VERSION IN /ETC/LSB-RELEASE"
 echo "###############################################################################"
 echo
-sudo sed -i 's/\(^DISTRIB_RELEASE=\).*/\1v20.3.3/' /etc/lsb-release
+sudo sed -i 's/\(^DISTRIB_RELEASE=\).*/\1v20.3.4/' /etc/lsb-release
 
 echo "###############################################################################"
 echo "###                LSB-RELEASE NUMBER UPDATED               ####"
