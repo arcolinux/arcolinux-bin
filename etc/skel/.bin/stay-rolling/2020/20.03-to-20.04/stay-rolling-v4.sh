6ed681652f50edfa52527cef34cde9993829b76d
Erik Dubois <erik.dubois@gmail.com>
2020-04-09 19:43:22 +0200

    version 11

--- etc/skel/.bin/stay-rolling/2020/20.03-to-20.04/stay-rolling-v4.sh
+++ etc/skel/.bin/stay-rolling/2020/20.03-to-20.04/stay-rolling-v4.sh
@@ -101,7 +101,7 @@ echo "##########################################################################
 echo "CHANGING VERSION IN /ETC/LSB-RELEASE"
 echo "###############################################################################"
 echo
-sudo sed -i 's/\(^DISTRIB_RELEASE=\).*/\1v20.4.10/' /etc/lsb-release
+sudo sed -i 's/\(^DISTRIB_RELEASE=\).*/\1v20.4.11/' /etc/lsb-release
 
 echo "###############################################################################"
 echo "###                LSB-RELEASE NUMBER UPDATED               ####"
