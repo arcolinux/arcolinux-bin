ae79b9f293a6eae6e3ac2d28acc82240508c3067
Erik Dubois <erik.dubois@gmail.com>
2020-05-02 12:27:19 +0200

    stay rolling

--- etc/skel/.bin/stay-rolling/2020/20.04-to-20.05/stay-rolling-v1.sh
+++ etc/skel/.bin/stay-rolling/2020/20.04-to-20.05/stay-rolling-v1.sh
@@ -58,40 +58,21 @@ echo "SPECIALITIES"
 echo "###############################################################################"
 echo
 echo "None"
-#sudo pacman -S --noconfirm --needed
 echo
 echo "###############################################################################"
 echo "REMOVALS"
 echo "###############################################################################"
 echo "We have removed these packages from the iso :"
 echo
-#echo "None"
-#function_remove ...
-echo "Decide whether you want to keep or remove the breeze package"
-echo "Breeze is a beautiful cursor for plasma but it also brings in a lot of dependencies"
-echo "Plasma users are advised to keep the breeze cursor - answer n or N"
-echo "Answer y or Y if you want to remove it"
-echo "Answer n or N if you want to keep it"
-read response
-if [[ "$response" == [yY] ]]; then
-  sudo pacman -Rs breeze --noconfirm
-fi
-echo "Decide whether you want remove xfce4-screensaver"
-echo "We experienced issues with screens going black because of this package"
-echo "Answer y or Y if you want to remove it"
-echo "Answer n or N if you want to keep it"
-read response
-if [[ "$response" == [yY] ]]; then
-  sudo pacman -Rs xfce4-screensaver --noconfirm
-fi
+echo "None"
 echo
 echo "###############################################################################"
 echo "INSTALLATIONS"
 echo "###############################################################################"
 echo "We have installed these packages on the iso :"
 echo
-echo "bibata-cursor-theme"
-sudo pacman -S --noconfirm --needed bibata-cursor-theme
+echo "polybar - you can now switch the bar in i3 with ATT"
+sudo pacman -S --noconfirm --needed polybar
 echo
 echo "###############################################################################"
 echo "###                   STAY-ROLLING DONE                     ####"
@@ -101,7 +82,7 @@ echo "##########################################################################
 echo "CHANGING VERSION IN /ETC/LSB-RELEASE"
 echo "###############################################################################"
 echo
-sudo sed -i 's/\(^DISTRIB_RELEASE=\).*/\1v20.4.11/' /etc/lsb-release
+sudo sed -i 's/\(^DISTRIB_RELEASE=\).*/\1v20.5.6/' /etc/lsb-release
 
 echo "###############################################################################"
 echo "###                LSB-RELEASE NUMBER UPDATED               ####"
