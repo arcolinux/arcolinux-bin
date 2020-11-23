54f53f230aca57545deca673ea2a6ffa749f5990
Erik Dubois <erik.dubois@gmail.com>
2020-11-23 14:53:22 +0100

    swapfile size

---
+++ etc/skel/.bin/main/change-the-size-of-your-current-swapfile.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+#set -e
+###############################################################################
+# Author	:	Erik Dubois
+# Website	:	https://www.erikdubois.be
+# Website	:	https://www.arcolinux.info
+# Website	:	https://www.arcolinux.com
+# Website	:	https://www.arcolinuxd.com
+# Website	:	https://www.arcolinuxb.com
+# Website	:	https://www.arcolinuxforum.com
+###############################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+###############################################################################
+
+  if test -f /swapfile 
+  then
+  	tput setaf 2
+  	echo "###############################################################################"
+  	echo "Let us increase the current size of your /swapfile"
+  	echo "###############################################################################"
+  	tput sgr0
+  else
+  	tput setaf 2
+  	echo "###############################################################################"
+  	echo "This script is meant for users who have already a /swapfile and want"
+  	echo "to increase the /swapfile"
+  	echo "###############################################################################"
+  	tput sgr0
+  	exit 1
+  fi
+
+echo
+echo "###############################################################################"
+echo "CHOOSE THE SIZE OF YOUR SWAPFILE"
+echo "EXPRESSED IN GB"
+echo "CHOOSE A NUMBER BETWEEN 1 AND 32"
+echo "###############################################################################"
+echo
+read choice
+
+if [ "$choice" -ge 1 ] && [ "$choice" -le 32 ]
+
+then
+	echo "###############################################################################"
+	echo "You have chosen "$choice" GB."
+	echo "###############################################################################"
+
+else 
+	echo "###############################################################################"
+	echo "Choose a number equal to or between 1-32"
+	echo "###############################################################################"
+
+fi
+
+echo
+echo "###############################################################################"
+echo "CHANGING SWAPFILE SIZE"
+echo "###############################################################################"
+echo
+echo "###############################################################################"
+echo "Turning off current swapfile"
+echo "###############################################################################"
+echo
+sudo swapoff -a
+echo
+echo "###############################################################################"
+echo "Removing current swapfile"
+echo "###############################################################################"
+echo
+sudo rm -f /swapfile
+echo
+echo "###############################################################################"
+echo "##Making new swapfile" ##"Change count to desired swapfile size in mb"
+echo "###############################################################################"
+echo
+sudo dd if=/dev/zero of=/swapfile bs=1G count=$choice status=progress
+echo
+echo "###############################################################################"
+echo "Changing permissions on swapfile"
+echo "###############################################################################"
+echo
+sudo chmod 600 /swapfile
+echo
+echo "###############################################################################"
+echo "Formating swapfile"
+echo "###############################################################################"
+echo
+sudo mkswap /swapfile
+echo
+echo "###############################################################################"
+echo "Activating swapfile"
+echo "###############################################################################"
+echo
+sudo swapon /swapfile
+echo
+echo "###############################################################################"
+echo "                  Finished - Please REBOOT your computer                       "
+echo "###############################################################################"
+echo
+
+
