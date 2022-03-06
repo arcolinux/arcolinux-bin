d091d3e665226738d45bfcc61d54043bb0b5ee52
Erik Dubois <erik.dubois@gmail.com>
2022-03-06 07:53:30 +0100

    nsswitch

--- etc/skel/.bin/main/samba/2-install-network-discovery-v4.sh
+++ etc/skel/.bin/main/samba/2-install-network-discovery-v4.sh
@@ -25,16 +25,22 @@ sudo pacman -S --noconfirm --needed nss-mdns
 #shares on a linux
 sudo pacman -S --noconfirm --needed gvfs-smb
 
-#change nsswitch.conf for access to nas servers
-#original line comes from the package filesystem
-#hosts: files mymachines myhostname resolve [!UNAVAIL=return] dns
-#ArcoLinux line
-#hosts: files mymachines resolve [!UNAVAIL=return] mdns dns wins myhostname
+tput setaf 5;echo "################################################################"
+echo "Change /etc/nsswitch.conf for access to nas servers"
+echo "We assume you are on ArcoLinux and have"
+echo "arcolinux-system-config-git or arcolinuxd-system-config-git"
+echo "installed. Else check and change the content of this file to your liking"
+echo "################################################################"
+echo;tput sgr0
 
-#first part
-sudo sed -i 's/files mymachines myhostname/files mymachines/g' /etc/nsswitch.conf
-#last part
-sudo sed -i 's/\[\!UNAVAIL=return\] dns/\[\!UNAVAIL=return\] mdns dns wins myhostname/g' /etc/nsswitch.conf
+# https://wiki.archlinux.org/title/Domain_name_resolution
+if [ -f /usr/local/share/arcolinux/nsswitch.conf ]; then
+	echo "Make backup and copy ArcoLinux conf over"
+	echo
+	sudo cp /etc/nsswitch.conf /etc/nsswitch.conf.bak
+	sudo cp /usr/local/share/arcolinux/nsswitch.conf /etc/nsswitch.conf
+fi
+
 echo "################################################################"
 echo "####       network discovery  software installed        ########"
 echo "################################################################"
